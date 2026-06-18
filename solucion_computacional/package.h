a2048f75b19511a99592c4ef852d6810f70c3813
CarlosMirabilis <elcarlos070708@gmail.com>
2026-06-18 12:34:34 -0600

    Estructura base: package.h y enums de estado de entrega del paquete

---
+++ solucion_computacional/package.h
@@ -0,0 +1,21 @@
+#ifndef PACKAGE_H
+#define PACKAGE_H
+#include <stdio.h>
+typedef enum {
+    REGISTERED,
+    PENDING_DELIVERY,
+    IN_ROUTE,
+    DELIVERED,
+    RETURNED
+} PackageStatus;
+typedef struct {
+    char code[10];
+    char clientId[10];
+    char recipientName[50];
+    char destination[50];
+    float weight;
+    int priority;
+    PackageStatus status;
+} Package;
+void printPackage(Package p);
+#endif
